1b6d75de15c19306e563e089cb52fcbebdb9ba87
tari2025 <denilestari272@gmail.com>
2025-05-15 10:30:44 +0700

    private dan public

---
+++ doubleLinkedList.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node
+{
+public:
+    int noMhs;
+    Node *next;
+    Node *prev;
+};
+
+class DoubleLinkedList
+{
+private:
+    Node *START;
+
+public:
+    DoubleLinkedList()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        string nm;
+        cout << "\nEnter the roll number of the student"
+    }
