091cd6b2cf65bf4e66e8be4febc23e4dc503cf2d
tari2025 <denilestari272@gmail.com>
2025-05-15 11:00:19 +0700

    step 9 : insert between current and current->next

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -62,7 +62,18 @@ public:
     Node *current = START;
     while (current->next != NULL && current->next->noMhs < nim)
     {
-        current
+        current = current->next;
     }
+
+    if current->next !=NULL && nim current->next->noMhs)
+        {
+            cout << "nDuplicate roll number not allowed" << endl;
+            return;
+        }
+
+    // step 9 : insert between current and current->next
+    newNode->next = current->next; // step 9a
+}
+}
 }
 }
